dd2ddc87fdef942f175fe3668e304efdbd7cdbe4
Aliaksei Barysionak <abarysionak@evolution.com>
2022-10-22 19:58:59 +0300

    init 5

--- src/index.js
+++ src/index.js
@@ -8,12 +8,16 @@ import Films from './pages/Cards';
 import Invoice from './pages/Invoice';
 import './main.css';
 
-
-const script = document.createElement('script');
-script.setAttribute('src','./script.js');
-document.head.appendChild(script);
-
 const rootElement = document.getElementById('root');
+
+(function(h,o,t,j,a,r){
+  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+  h._hjSettings={hjid:3214164,hjsv:6};
+  a=o.getElementsByTagName('head')[0];
+  r=o.createElement('script');r.async=1;
+  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+  a.appendChild(r);
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 
 ReactDOM.render(
   <BrowserRouter>
