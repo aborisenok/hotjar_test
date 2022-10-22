8e23c5ac85915b337aab4d808f71842b39eec6cb
Aliaksei Barysionak <abarysionak@evolution.com>
2022-10-22 21:57:34 +0300

    init 8

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const rootElement = document.getElementById('root');
 
 (function(h,o,t,j,a,r){
   h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-  h._hjSettings={hjid:3214164,hjsv:6};
+  h._hjSettings={hjid:3209086,hjsv:6};
   a=o.getElementsByTagName('head')[0];
   r=o.createElement('script');r.async=1;
   r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
